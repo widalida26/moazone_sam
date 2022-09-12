e3323882793bada82a7e241034d92b88f6e4440a
widalida26 <widalida26@gmail.com>
2022-09-13 03:30:06 +0900

    add : workflow for sam

--- functions/challenge/challenge.py
+++ functions/challenge/challenge.py
@@ -4,31 +4,31 @@ import boto3
 
 def handler(event, context):
     body_data = json.loads(event['body'])
-    print(body_data)
     challenge_data = body_data['challengeData']
-    print(challenge_data)
 
     client = boto3.client('cognito-idp', region_name='ap-northeast-2')
-    try:  
-        response = client.respond_to_auth_challenge (
-            ChallengeName = 'CUSTOM_CHALLENGE',
-            ClientId = os.environ.get('CLIENT_ID'),
-            ChallengeResponses = {
-                'USERNAME': challenge_data['username'],
-                'ANSWER': challenge_data['answer'],
-            },
-            Session = challenge_data['session']
-        )
-        accessToken = response.AuthenticationResult.accessToken
+    response = client.respond_to_auth_challenge (
+        ChallengeName = 'CUSTOM_CHALLENGE',
+        ClientId = os.environ.get('CLIENT_ID'),
+        ChallengeResponses = {
+            'USERNAME': challenge_data['username'],
+            'ANSWER': challenge_data['answer'],
+        },
+        Session = challenge_data['session']
+    )
+    print(response)
+    authResult = response['AuthenticationResult']
+    print(authResult)
+    try:
         return {
             'statusCode': 200,
             'headers': {
                 'Access-Control-Allow-Headers': 'Content-Type',
                 'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS, POST, GET'
+                'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
             },
-            'body': json.dumps(accessToken) 
-        }
+            'body': json.dumps({"accessToken" : authResult['AccessToken'], "username" : challenge_data['username']}) 
+            }
     except:
         return {
             'statusCode': 200,
--- functions/lambda-triggers/verify-auth-challenge-response/verify-auth-challenge-response.ts
+++ functions/lambda-triggers/verify-auth-challenge-response/verify-auth-challenge-response.ts
@@ -3,12 +3,12 @@
 
 import { VerifyAuthChallengeResponseTriggerHandler } from 'aws-lambda';
 
-export const handler: VerifyAuthChallengeResponseTriggerHandler = async event => {
-    const expectedAnswer = event.request.privateChallengeParameters!.secretLoginCode; 
-    if (event.request.challengeAnswer === expectedAnswer) {
-        event.response.answerCorrect = true;
-    } else {
-        event.response.answerCorrect = false;
-    }
-    return event;
+export const handler: VerifyAuthChallengeResponseTriggerHandler = async (event) => {
+  const expectedAnswer = event.request.privateChallengeParameters!.secretLoginCode;
+  if (event.request.challengeAnswer === expectedAnswer) {
+    event.response.answerCorrect = true;
+  } else {
+    event.response.answerCorrect = false;
+  }
+  return event;
 };
